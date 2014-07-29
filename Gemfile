9fcea71d538969418efd3713c0af892afde16345
briank84 <briankim84@gmail.com>
2014-07-29 11:58:53 -0400

    basic sinatra layout

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# webapp framework
+gem "sinatra"
---
+++ app.rb
@@ -0,0 +1,9 @@
+require "sinatra"
+
+set :bind, '0.0.0.0'
+set :server, 'webrick'
+
+get '/' do
+    erb :"index", layout: :"layouts/main"
+    @hello = "Hello World"
+end
